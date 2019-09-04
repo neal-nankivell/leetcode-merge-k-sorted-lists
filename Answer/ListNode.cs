3b1f844e1db9c87b9124f497f3d277843a05420f
neal.nankivell <neal.nankivell@gmail.com>
2019-09-04 16:07:30 +0200

    Added solution template

---
+++ Answer/ListNode.cs
@@ -0,0 +1,9 @@
+namespace Answer
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ Answer/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Answer
+{
+    /*
+    Merge k sorted linked lists and return it as one sorted list.
+    Analyze and describe its complexity.
+    */
+    public class Solution
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
